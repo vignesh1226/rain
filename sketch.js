20775cc4339d96a6d26d25e3f0f921c67ff574b8
vignesh1226 <61319256+vignesh1226@users.noreply.github.com>
2020-03-16 14:45:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,6 +14,21 @@ function setup() {
   c = new Raindrop(30,40,20,PI);
   d = new Raindrop(40,50,20,PI);
   e = new Raindrop(30,60,20,PI);
+  f = new Raindrop(20,30,20,PI);
+  g = new Raindrop(30,30,20,PI);
+  h = new Raindrop(30,40,20,PI);
+  i = new Raindrop(40,50,20,PI);
+  j = new Raindrop(30,60,20,PI);
+  k = new Raindrop(20,30,20,PI);
+  l = new Raindrop(30,30,20,PI);
+  m = new Raindrop(30,40,20,PI);
+  n = new Raindrop(40,50,20,PI);
+  o = new Raindrop(30,60,20,PI);
+  p = new Raindrop(20,30,20,PI);
+  q = new Raindrop(30,30,20,PI);
+  r = new Raindrop(30,40,20,PI);
+  s = new Raindrop(40,50,20,PI);
+  t = new Raindrop(30,60,20,PI);
 
 }
 
@@ -25,4 +40,19 @@ function draw() {
   c.display();
   d.display();
   e.display();
+  f.display();
+  g.display();
+  h.display();
+  i.display();
+  j.display();
+  k.display();
+  l.display();
+  m.display();
+  n.display();
+  o.display();
+  p.display();
+  q.display();
+  r.display();
+  s.display();
+  t.display();
 }
