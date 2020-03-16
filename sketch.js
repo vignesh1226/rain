190f3b4f80a22090df3e707c64a428d3b7d57d05
vignesh1226 <61319256+vignesh1226@users.noreply.github.com>
2020-03-16 14:47:24 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 
 function setup() {
-  var canvas = createCanvas(1200,400);
+  var canvas = createCanvas(400,400);
   engine = Engine.create();
   world = engine.world;
 
@@ -15,20 +15,20 @@ function setup() {
   d = new Raindrop(40,50,20,PI);
   e = new Raindrop(30,60,20,PI);
   f = new Raindrop(20,30,20,PI);
-  g = new Raindrop(30,30,20,PI);
-  h = new Raindrop(30,40,20,PI);
-  i = new Raindrop(40,50,20,PI);
-  j = new Raindrop(30,60,20,PI);
-  k = new Raindrop(20,30,20,PI);
-  l = new Raindrop(30,30,20,PI);
-  m = new Raindrop(30,40,20,PI);
-  n = new Raindrop(40,50,20,PI);
-  o = new Raindrop(30,60,20,PI);
-  p = new Raindrop(20,30,20,PI);
-  q = new Raindrop(30,30,20,PI);
-  r = new Raindrop(30,40,20,PI);
-  s = new Raindrop(40,50,20,PI);
-  t = new Raindrop(30,60,20,PI);
+  g = new Raindrop(30,40,20,PI);
+  h = new Raindrop(30,80,20,PI);
+  i = new Raindrop(40,20,20,PI);
+  j = new Raindrop(100,90,20,PI);
+  k = new Raindrop(54,74,20,PI);
+  l = new Raindrop(31,39,20,PI);
+  m = new Raindrop(80,356,20,PI);
+  n = new Raindrop(82,208,20,PI);
+  o = new Raindrop(78,34,20,PI);
+  p = new Raindrop(67,42,20,PI);
+  q = new Raindrop(74,42,20,PI);
+  r = new Raindrop(30,34,20,PI);
+  s = new Raindrop(340,150,20,PI);
+  t = new Raindrop(378,360,20,PI);
 
 }
 
